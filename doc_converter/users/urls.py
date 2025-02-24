99ff4ef361203c18a64f32a76437fe7610c3512f
Snipermania <82090671+Snipermania@users.noreply.github.com>
2025-02-24 20:13:51 +0530

    Delete doc_converter directory

--- doc_converter/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserCreateView, LoginView, LogoutView
-
-urlpatterns = [
-    path("register/", UserCreateView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-]
